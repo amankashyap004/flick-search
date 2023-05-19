6db9be624da95cf2d7858cfed24c64e5dd352898
amankashyap004 <amankashyap004@gmail.com>
2023-05-19 19:36:23 +0530

    TMDB API Testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
+import { fetchDataFromApi } from "./utils/api";
 
 function App() {
-   return <div>app</div>;
+   useEffect(() => {
+      apiTesting();
+   }, []);
+
+   const apiTesting = () => {
+      fetchDataFromApi("?movie/popular").then((res) => {
+         console.log(res);
+      });
+   };
+   return <div className="App">app</div>;
 }
 
 export default App;
