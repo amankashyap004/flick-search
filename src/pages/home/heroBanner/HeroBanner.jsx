d2ebdf9c4ea97ef0a9adc79536a65eafceb7a3e1
amankashyap004 <amankashyap004@gmail.com>
2023-05-21 19:30:08 +0530

    hero banner

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 import "./style.scss";
 import useFetch from "../../../hooks/useFetch";
 
@@ -7,11 +8,12 @@ const HeroBanner = () => {
    const [background, setBackground] = useState("");
    const [query, setQuery] = useState("");
    const navigate = useNavigate();
+   const { url } = useSelector((state) => state.home);
 
    const { data, loading } = useFetch("/movie/upcoming");
 
    useEffect(() => {
-      const bg = data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
+      const bg = url.backdrop + data?.results?.[Math.floor(Math.random() * 20)]?.backdrop_path;
       setBackground(bg);
    }, [data]);
 
