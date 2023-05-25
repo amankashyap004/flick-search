0e08b76862a49848a89d08b882824fd725b9c8f7
amankashyap004 <amankashyap004@gmail.com>
2023-05-25 17:47:44 +0530

    import file for header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { HiOutlineSearch } from "react-icons/hi";
+import { SlMenu } from "react-icons/sl";
+import { VscChromeClose } from "react-icons/vsc";
+import { useNavigate, useLocation } from "react-router-dom";
 import "./style.scss";
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+import logo from "../../assets/movix-logo.svg";
 
 const Header = () => {
    return <div>Header</div>;
