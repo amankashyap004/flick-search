e7f133689c122de5ca3eb9de20e9e304951de996
amankashyap004 <amankashyap004@gmail.com>
2023-05-30 17:56:59 +0530

    Switch Tabs

---
+++ src/components/switchTabs/SwitchTabs.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./style.scss";
+
+const SwitchTabs = () => {
+   return <div>SwitchTabs</div>;
+};
+
+export default SwitchTabs;
--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,11 +1,15 @@
-import React, { useState } from "react";
+import React from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import SwitchTabs from "../../../components/switchTabs/switchTabs";
 
 const Trending = () => {
+   const onTabChange = (tab) => {};
+
    return (
       <div className="carouselSection">
          <ContentWrapper>
             <span className="carouselTitle">Trending</span>
+            <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
          </ContentWrapper>
       </div>
    );
