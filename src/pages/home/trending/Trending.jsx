357883d1750cb5aca5e2e7d309f22c3208b58628
amankashyap004 <amankashyap004@gmail.com>
2023-06-02 21:54:35 +0530

    Carousel

--- src/pages/home/trending/Trending.jsx
+++ src/pages/home/trending/Trending.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import SwitchTabs from "../../../components/switchTabs/switchTabs";
+import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
+import Carousel from "../../../components/carousel/Carousel";
 import useFetch from "../../../hooks/useFetch";
 
 const Trending = () => {
@@ -17,6 +18,7 @@ const Trending = () => {
             <span className="carouselTitle">Trending</span>
             <SwitchTabs data={["Day", "Week"]} onTabChange={onTabChange} />
          </ContentWrapper>
+         <Carousel data={data?.results} loading={loading} />
       </div>
    );
 };
