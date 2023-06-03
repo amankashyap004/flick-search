6be36dfd540f27292c5b0304ebf8f1a8947600c9
amankashyap004 <amankashyap004@gmail.com>
2023-06-03 17:12:11 +0530

    fix some spell mistake

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -52,7 +52,7 @@ const Carousel = ({ data, loading, endpoint, title }) => {
                onClick={() => navigation("left")}
             />
             <BsFillArrowRightCircleFill
-               className="carouselRighttNav arrow"
+               className="carouselRightNav arrow"
                onClick={() => navigation("right")}
             />
             {!loading ? (
