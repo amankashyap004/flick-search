28ed697adc311f2b50a63ab386d0589bf91f8574
amankashyap004 <amankashyap004@gmail.com>
2023-06-04 16:37:07 +0530

    Details

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import "./style.scss";
+import useFetch from "../../hooks/useFetch";
 
 const Details = () => {
+   const { mediaType, id } = useParams();
+   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+   const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
+
    return <div>Details</div>;
 };
 
