85e584485db2e885b53119a79c8f38a27a999935
amankashyap004 <amankashyap004@gmail.com>
2023-06-05 17:51:56 +0530

    play btn

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,13 +2,18 @@ import React from "react";
 import { useParams } from "react-router-dom";
 import "./style.scss";
 import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
 
 const Details = () => {
    const { mediaType, id } = useParams();
    const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
    const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
 
-   return <div>Details</div>;
+   return (
+      <div>
+         <DetailsBanner />
+      </div>
+   );
 };
 
 export default Details;
