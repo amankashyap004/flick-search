b2f337a608fc4f727da72d5d08d6cbcb73fa2bdf
amankashyap004 <amankashyap004@gmail.com>
2023-06-12 17:36:43 +0530

    cteate Recommendation

--- src/pages/details/carousels/Recommendation.jsx
+++ src/pages/details/carousels/Recommendation.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
-const Recommendation = () => {
-   return <div>Recommendation</div>;
+import Carousel from "../../../components/carousel/Carousel";
+import useFetch from "../../../hooks/useFetch";
+
+const Recommendation = ({ mediaType, id }) => {
+   const { data, loading, error } = useFetch(`/${mediaType}/${id}/recommendations`);
+
+   return (
+      <Carousel
+         title="Recommendations"
+         data={data?.results}
+         loading={loading}
+         endpoint={mediaType}
+      />
+   );
 };
 
 export default Recommendation;
