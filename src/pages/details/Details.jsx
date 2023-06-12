19dedf52d43ffd2a709cc80d1b9aa729f862c8fd
amankashyap004 <amankashyap004@gmail.com>
2023-06-12 17:38:50 +0530

    Recommendation

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -6,6 +6,7 @@ import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
 import Cast from "./cast/Cast";
 import VideosSection from "./videosSection/VideosSection";
+import Recommendation from "./carousels/Recommendation";
 
 const Details = () => {
    const { mediaType, id } = useParams();
@@ -17,6 +18,7 @@ const Details = () => {
          <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
          <Cast data={credits?.cast} loading={creditsLoading} />
          <VideosSection data={data} loading={loading} />
+         <Recommendation mediaType={mediaType} id={id} />
       </div>
    );
 };
