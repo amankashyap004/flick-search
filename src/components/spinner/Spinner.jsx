eb5526ccf1863ff1d4f69cacc2cef3ece0fd9c72
amankashyap004 <amankashyap004@gmail.com>
2023-06-13 19:05:13 +0530

    Spinner

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import "./style.scss";
 
-const Spinner = () => {
-   return <div>Spinner</div>;
+const Spinner = ({ initial }) => {
+   return (
+      <div className={`loadingSpinner ${initial ? "initial" : ""}`}>
+         <svg className="spinner" viewBox="0 0 50 50">
+            <circle className="path" cx="25" cy="25" r="20" fill="none" strokeWidth="5"></circle>
+         </svg>
+      </div>
+   );
 };
 
 export default Spinner;
